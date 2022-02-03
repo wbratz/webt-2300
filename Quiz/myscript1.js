5695fb534319bfcae5a5f18a49426ece9b83b546
Billy <wbratz1@gmail.com>
2022-02-02 20:01:24 -0500

    .

---
+++ Quiz/myscript1.js
@@ -0,0 +1,18 @@
+function myFunction1() {
+    document.getElementById("demo2").innerHTML = "Paragraph changed!!";
+  }
+  
+
+  function func2() {
+    var X = 5; var Y = 3; var Z = 2;
+    var result = ( X + Y ) / Z * X;
+
+    console.log(result)
+  }
+
+  function func3() {
+    var X = 15; var Y = 3; var Z = 2;
+    var result = X % Y * Z; 
+
+    console.log(result)
+  }
